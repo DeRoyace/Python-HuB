798c5de1b521ba9b492abd8499f35e3fa4e32f81
Rohit Roy <creative.roy2003@gmail.com>
2022-02-25 23:40:27 +0530

    two num calculation

---
+++ calculate.py
@@ -0,0 +1,11 @@
+n1 = input("Enter number 1: ")
+n2 = input("Enter number 2: ")
+print("Sum =", int(n1) + int(n2))
+
+
+a = 12
+b = 9
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)

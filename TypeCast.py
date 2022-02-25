8459056a87a8857c155bc7716a626fa57ff04658
Rohit Roy <creative.roy2003@gmail.com>
2022-02-25 23:47:48 +0530

    type casting

---
+++ TypeCast.py
@@ -0,0 +1,6 @@
+a = 123
+b = "123"
+c = int(b) # on type casting we are assigning string value of b to c as int type
+
+print(a==b) # String numbers can't be compare with Integer so it gives False as output
+print(a==c) # True

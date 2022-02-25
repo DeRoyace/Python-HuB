7149be1db590f5987239a33fb766602455619090
Rohit Roy <creative.roy2003@gmail.com>
2022-02-25 23:43:15 +0530

    get data type of variable

---
+++ dataType.py
@@ -0,0 +1,15 @@
+a = 12
+b = 78.9
+c = 'R'
+d = "hello world"
+e = None
+f = False
+
+print(a, b, c, d, e, f)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))

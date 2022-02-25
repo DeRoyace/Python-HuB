b7bdf263edf1ffea71aa134a8cf731535f962875
Rohit Roy <creative.roy2003@gmail.com>
2022-02-25 23:44:21 +0530

    examples of esc ape sequences

---
+++ escapeSequence.py
@@ -0,0 +1,15 @@
+print("We insert quotes like this -  \"Hello\"")
+print("\'Single quotes\'")
+print("Tab is inserted -\t4 space tab")
+print("Insert black slash - \\hehe")
+
+# Escape 
+# Sequences	            Description 
+#   \n 	    Inserts a new line in the text at the point
+#   \\	    Inserts a backslash character in the text at the point
+#   \"	    Inserts a double quote character in the text at that point
+#   \'	    Inserts a single quote character in the text at that point
+#   \t	    Inserts a tab in the text at that point
+#   \f	    Inserts a form feed in the text at that point
+#   \r	    Inserts a carriage return in the text at that point
+#   \b	    Inserts a backspace in the text at that point

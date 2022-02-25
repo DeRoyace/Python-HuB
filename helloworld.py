7b4c445089e0f75b3200637aaa1b61f7216d73e3
Rohit Roy <creative.roy2003@gmail.com>
2022-02-25 23:45:09 +0530

    print strings and time module

---
+++ helloworld.py
@@ -0,0 +1,30 @@
+import time  #importing time module for our program
+print("Waiting for 3 seconds...")
+time.sleep(3) #using this function-> terminal is waiting for 3 seconds to print the string.
+
+print("\nHello world ! What's up this is my first python program.")
+
+
+print('''\nAs mean as a wolf, as sharp as a tooth
+As deep as a bite, as dark as the night
+As sweet as a song, as right as a wrong
+As long as a road, as ugly as a toad
+As pretty as a picture hanging from a fixture
+Strong like a family, strong as I wanna be
+Bright as day, as light as play
+As hard as nails, as grand as a whale''')
+
+# Bring the cursor on the first line then alt + shift + bring the cursor on last line. On doing this multiple cursors will be formed in each line
+# After that if we press END key, the cursors will move to end of each line.
+# Similarly, if we press HOME key, the cursors will move to front of each line.
+
+print("\nAs warm as the sun, as silly as fun")
+print("As cool as a tree, as scary as the sea")
+print("As hot as fire, cold as ice")
+print("Sweet as sugar and everything nice")
+print("As old as time, as straight as a line")
+print("As royal as a queen, as buzzed as a bee")
+print("As stealth as a tiger, smooth as a glider")
+print("Pure as a melody, pure as I wanna be")
+
+'''multi line comment'''

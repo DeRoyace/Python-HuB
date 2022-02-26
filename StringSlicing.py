6af5c3bf3a11ae54ae00a1770ae1837faa1e6440
Rohit Roy <creative.roy2003@gmail.com>
2022-02-26 21:53:15 +0530

    printing out substrings

---
+++ StringSlicing.py
@@ -0,0 +1,6 @@
+str =  "De Royace"
+#Index: 012345678
+print(str[3])
+print(str[3 : 6]) # printing string characters from index 3 to 5
+print(str[6:]) # prints all characters from index 6
+print(str[:6]) # prints all characters before index 6

41cf7b576d8858a42900ef908345943fd8fac5f8
Rohit Roy <creative.roy2003@gmail.com>
2022-02-26 21:52:01 +0530

    updated

--- escapeSequence.py
+++ escapeSequence.py
@@ -2,6 +2,10 @@ print("We insert quotes like this -  \"Hello\"")
 print("\'Single quotes\'")
 print("Tab is inserted -\t4 space tab")
 print("Insert black slash - \\hehe")
+print("Inserting a backspace here -->\bspace")
+print("Inserting a carriage return \rReturn")
+# what we write after \r it will overwrite in the string before \r
+print("Inserting form feed here\ffeedback: excellent!!")
 
 # Escape 
 # Sequences	            Description 

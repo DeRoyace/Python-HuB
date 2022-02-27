e0cb2720b6216e311e0179f29044d535288f51b6
Rohit Roy <creative.roy2003@gmail.com>
2022-02-28 00:02:35 +0530

    time module in Python

---
+++ TimeModule.py
@@ -0,0 +1,4 @@
+import time  #importing time module for our program
+print("Waiting for 3 seconds...")
+time.sleep(3) #using this function-> terminal is waiting for 3 seconds to print the string.
+print("Time taken to print this String is 3 seconds")

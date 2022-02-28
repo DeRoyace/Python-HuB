79e15f330798b2316794e94b71694144af16acea
Rohit Roy <creative.roy2003@gmail.com>
2022-02-28 23:53:21 +0530

    computing two integers

--- calculate.py
+++ calculate.py
@@ -1,10 +1,12 @@
 n1 = input("Enter number 1: ")
 n2 = input("Enter number 2: ")
 print("Sum =", int(n1) + int(n2))
-
+# In the above statements we are type casting n1 and n2 b'coz by default python takes String inputs
+# so if we add them without type casting, then the numbers will be concatenated rather than added. 
 
 a = 12
 b = 9
+print("\na = ", a, "   b = ", b)
 print("a + b = ", a + b)
 print("a - b = ", a - b)
 print("a * b = ", a * b)

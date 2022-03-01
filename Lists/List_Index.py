24fe83c534f1a942a006749b8e58d27fd5dc6645
Rohit Roy <creative.roy2003@gmail.com>
2022-03-01 23:49:32 +0530

    access list by index

---
+++ Lists/List_Index.py
@@ -0,0 +1,11 @@
+# This list given below is storing names of several countries 
+country = ["USA", "Australia", "India", "Japan", "New Zealand", "Germany", "France", "Russia"]
+# Index :    0         1          2        3           4            5          6         7
+# (-ve) :   -8        -7         -6       -5          -4           -3         -2        -1
+
+print(country) # prints the whole list
+print(country[2]) # prints the list value at index 2; like these we can access list items
+print(country[3:]) # prints the lists starting from index 1
+print(country[:4]) # print the lists before index 4
+print(country[5:7]) # prints the lists from index 5 to 6
+print(country[-6:-1]) # prints the list from negative index -6 to -2

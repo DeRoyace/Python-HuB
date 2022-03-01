46f2afe58d22e9c625a7854b6404a4772fad65a5
Rohit Roy <creative.roy2003@gmail.com>
2022-03-01 23:50:04 +0530

    remove list elements

---
+++ Lists/Remove_List_items.py
@@ -0,0 +1,8 @@
+food = ["Sanswich", "Pizza", "Omlette", "Juice", "Sausages", "Coffee"]
+food.pop() # pops out/ removes the item from last
+# we can also give indexes to remove items like food.pop(2)
+print(food)
+food.remove("Omlette") # removes the specified item name from the list
+print(food)
+food.clear() # deletes all list items
+print(food)

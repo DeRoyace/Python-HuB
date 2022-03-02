585c2472ebe0af3ac7043f95a4e765bd75521e9e
Rohit Roy <creative.roy2003@gmail.com>
2022-03-02 22:59:00 +0530

    count list length and duplicate items

---
+++ Lists/Count_List_items.py
@@ -0,0 +1,3 @@
+myList = ['T-shirt', 'Jeans', 'Snickers', 'Glasses', 'Smart-Band', "Jeans"]
+print(len(myList)) # prints the number of items present in the list
+print(myList.count("Jeans")) # prints no. of times 'Jeans' is present

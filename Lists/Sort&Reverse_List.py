d1ad2199b471ca1a1865a1b290f7ca251d23d56f
Rohit Roy <creative.roy2003@gmail.com>
2022-03-02 22:59:37 +0530

    sorting & reversing list items

--- Lists/Sort&Reverse_List.py
+++ Lists/Sort&Reverse_List.py
@@ -1,5 +1,9 @@
 nums = [12, 23, 14, 56, 7, 21, 98, 11, 100]
 nums.reverse() # reverses the list
 print(nums)
-nums.sort() # sorts the list
+nums.sort() # sorts the list in ascending order
 print(nums)
+
+object = ["Table", "Chair", "Bed", "Cupboard", "Sofa"]
+object.sort() # sorts the list items in alphabetical order of first character
+print(object)

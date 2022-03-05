7fe3a24e78b07a95561cd107cdd22dde1e52c52a
Rohit Roy <creative.roy2003@gmail.com>
2022-03-05 23:59:29 +0530

    boolean e.g

---
+++ Boolean.py
@@ -0,0 +1,16 @@
+from re import A
+print(bool(True))
+print(bool(A))
+print(bool(123))
+print(bool("Hello Guys!"))
+print(bool(('tupples', 97.89, 574)))
+print(bool(['Keyboard', 'Mouse']))
+print(bool({'qwerty', 'abcd'}))
+print() # giving a line gap
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))

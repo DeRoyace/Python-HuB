f217a38d5689c7463deab7cbdfe6be641c01cf22
Rohit Roy <creative.roy2003@gmail.com>
2022-03-06 00:00:07 +0530

    Dictionaries basic info

---
+++ Dictionaries/Basics.py
@@ -0,0 +1,9 @@
+# A Dictionary is a collection of key: value pairs 
+# It is ordered, i.e can be access by index 
+# It can also be modified 
+wiki = {"Python" : "It is a high-level, intepreted object-oriented programming language.",
+        "Google" : "A search engine",
+        "Github" : "GitHub is a web-based version control repository on the Internet with a hosting service.",
+        "Linux"  : "An open-source unix like OS based on  Kernel"
+        }
+print(wiki.items()) # prints the whole dictionary

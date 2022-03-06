86ef20fbac97abed123d64e072cb958b32848e3b
Rohit Roy <creative.roy2003@gmail.com>
2022-03-06 23:29:09 +0530

    copying dictionary items

---
+++ Dictionaries/Copy_dictionary.py
@@ -0,0 +1,7 @@
+dict = {"GUI" : "Graphical User Interface",
+        "AR"  : "Augmented Reality",
+        "WWW" : "World Wide Web"
+        }
+
+newDict = dict.copy() # like these we copy a dictionary
+print(newDict)

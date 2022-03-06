df926da76de24336d89d6691c7cc660ad138d36c
Rohit Roy <creative.roy2003@gmail.com>
2022-03-06 23:32:45 +0530

    sets basics

---
+++ Sets/Basics.py
@@ -0,0 +1,10 @@
+# Sets are collection of unique elements (i.e non-repetitive)
+# It is unordered
+# It cannot be access by index
+# We cannot edit set items; but we can add & remove items
+# We cannot have duplicate items
+
+setVal = {786, 98.76, "Rails", "Rust", True}
+print(setVal)
+print("Type of variable setVal:", type(setVal))
+print("Length of the set:", len(setVal))

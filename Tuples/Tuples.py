4848827f66d7071bf66ac047eade77bae18b5605
Rohit Roy <creative.roy2003@gmail.com>
2022-03-10 21:55:10 +0530

    basics of tuples

--- Tuples/Tuples.py
+++ Tuples/Tuples.py
@@ -9,6 +9,27 @@ point = (7, 9)
 print(point)
 print(point[1]) # we can access tuple elemnents by their index.
 
-axb = [(1,2), (1,3), (2,3)] # tuples inside a list
-axb[1] = (4,5) # changing list tuple
-print(axb)
+mix = (2022, "hello", 97.89, True, [7, 9], True, 2022)
+# tuples can store different data types and duplicate values. 
+print(mix)
+print(type(mix))
+print("Index of 2022 is ", mix.index(2022)) # prints the first index
+
+num = 9, 10, 7, 8, 2, 6, 12 # we can also create tuple like these
+print(num)
+print(type(num))
+
+nil = () # an empty tuple
+print(nil, type(nil))
+
+val = (99) # this is not a tuple, it will act a integer
+print(val, type(val))
+
+val2 = (98,) # this will act as tuple
+print(val2, type(val2))
+
+arr = ([4, 3]) # this will act as list
+print(arr, type(arr))
+
+brr = ([1, 6],)  # this is a tuple
+print(brr, type(brr))
